4e70c3dd92b83fade678eb544c893c14ecd9cca4
james <jameshoggard.dev@gmail.com>
2021-05-06 17:10:25 -0400

    commit tree 9caa3e470195b9a84ef10079781a6042f27b3e89

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Your code here
+function mapToNegativize(arr){
+    let new_arr = [];
+    for(let i=0; i<arr.length; i++){
+        new_arr.push(-1 * arr[i])
+    }
+    return new_arr;
+}
+
+function mapToNoChange(arr){
+    let new_arr = [];
+    for(let i=0; i<arr.length; i++){
+        new_arr.push(arr[i])
+    }
+    return new_arr;
+}
+
+function mapToDouble(arr){
+    const new_arr = arr.map(num => num * 2)
+    return new_arr;
+}
+
+function mapToSquare(arr){
+    let new_arr = [];
+    for(let i=0; i<arr.length; i++){
+        new_arr.push(arr[i]*arr[i])
+    }
+    return new_arr;
+}
+
+function reduceToTotal(arr, start=0) {
+    let total = start
+    for (let i = 0; i < arr.length; i++ ) {
+      total = total + arr[i]
+    }
+    return total
+  }
+  
+  function reduceToAllTrue(arr) {
+    for (let i = 0; i< arr.length; i++ ) {
+      if (!arr[i]) return false
+    }
+    return true
+  }
+
+  function reduceToAnyTrue(arr){
+      for(let i=0; i< arr.length; i++){
+          if(arr[i]) return true
+      }
+      return false;
+  }
